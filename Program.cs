c5469d5b7a94bdd05ee4dd1cfdb5fe16f4a5e595
johan suarez <jhoanseb1992@gmail.com>
2024-01-29 20:48:55 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace CliApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0 || args[0] == null)
+            {
+                Console.WriteLine("Please provide a file name");
+                return 1;
+            }
+            string fileName = args[0];
+
+            Console.WriteLine($"file name: {fileName}");
+            return 0;
+        }
+    }
+}
