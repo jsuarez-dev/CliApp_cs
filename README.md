a4dc7449aa92cd089dc75dab1628a6810d487768
johan suarez <jhoanseb1992@gmail.com>
2024-02-08 21:53:58 +1100

    re factor and add mode significade to my app

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 This a CLI commada that takes a text file and parse the letter to create a Trie of if. Then, it can make predictions base on the word save on the tried
 This project is develop in C# using .Net 8 Core.
 
+
+
+# Acknowledgments
+- https://github.com/matthewreagan/WebstersEnglishDictionary?tab=readme-ov-file
+
+From Project Gutenberg:
+This eBook is for the use of anyone anywhere at no cost and with almost no restrictions whatsoever. You may copy it, give it away or re-use it under the terms of the Project Gutenberg License included with this eBook or online at www.gutenberg.net
--- cli/Program.cs
+++ cli/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Text.Json;
 using MyDataStructures;
 
 namespace CliApp
@@ -7,72 +8,100 @@ namespace CliApp
     {
         static int Main(string[] args)
         {
-            if (args.Length == 0 || args[0] == null)
-            {
-                Console.WriteLine("Please provide a file name");
-                return 1;
-            }
-            string fileName = args[0];
+            string fileNameDictionary = "data/dictionary_compact.json";
 
-            TextFile textFile = new TextFile(fileName);
-            textFile.addWordsToTrie();
-
+            EnglishDictionary myDic = new EnglishDictionary(fileNameDictionary);
             bool flag_continue = true;
+
             string word = "";
 
-            Console.WriteLine("Start");
+            Console.Clear();
+            Console.WriteLine("Please provide a word:");
+
+            string[] prediction = [];
             do
             {
                 if (!Console.KeyAvailable)
                 {
-                    word += Console.ReadKey(true).Key;
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                    if (keyInfo.Key == ConsoleKey.Enter || keyInfo.Key == ConsoleKey.Spacebar || keyInfo.Key == ConsoleKey.Escape)
+                    {
+                        flag_continue = false;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.Backspace)
+                    {
+                        if (word.Length > 0)
+                        {
+                            word = word.Substring(0, word.Length - 1);
+                        }
+                    }
+                    else
+                    {
+                        word += keyInfo.Key.ToString().ToLower();
+                    }
+                    // clear the console
+                    if (prediction.Length > 0)
+                    {
+                        for (int i = 0; i < prediction.Length; i++)
+                        {
+                            Console.SetCursorPosition(0, Console.CursorTop - 1);
+                            Console.WriteLine(new string(' ', Console.WindowWidth));
+                            Console.SetCursorPosition(0, Console.CursorTop - 1);
+                        }
+                    }
                 }
-                Console.WriteLine(word + "\r");
+                Console.WriteLine(word);
+
+                prediction = myDic.getPrediction(word);
+
+                if (prediction.Length > 0)
+                {
+                    if (prediction.Length > 10)
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.WriteLine("-> " + prediction[i]);
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < prediction.Length; i++)
+                        {
+                            Console.WriteLine("-> " + prediction[i]);
+                        }
+                    }
+                }
             } while (flag_continue);
 
+            string definition = myDic.getDefinition(word);
+            Console.WriteLine("Definition:");
+            Console.WriteLine(definition);
 
-            if (word.Equals(""))
-            {
-                Console.WriteLine("Please provide a word");
-                return 1;
-            }
-
-            string[] prediction = textFile.getPrediction(word);
-
-            if (prediction.Length > 0)
-            {
-                foreach (string w in prediction)
-                {
-                    Console.WriteLine(w);
-                }
-            }
 
             return 0;
         }
     }
 
-    class TextFile
+    class EnglishDictionary
     {
-        public string[]? words;
-        public string fileName { get; set; }
-        public string? fileContent { get; set; }
-        private string filePath;
-        public Trie trie;
+        public Dictionary<string, string>? dic;
+        private Trie trie;
 
-        public TextFile(string fileName)
+        public EnglishDictionary(string fileName)
         {
-            this.fileName = fileName;
-            this.filePath = Path.GetFullPath(fileName);
-            if (File.Exists(this.filePath))
+            string filePath = Path.GetFullPath(fileName);
+            if (File.Exists(filePath))
             {
-                this.fileContent = File.ReadAllText(this.filePath);
-                this.getWords();
+                string text = File.ReadAllText(filePath);
+                this.dic = JsonSerializer.Deserialize<Dictionary<string, string>>(text);
             }
             else
             {
-                Console.WriteLine($"File {this.filePath} does not exist");
+                Console.WriteLine($"File {filePath} does not exist");
             }
             this.trie = new Trie();
+            this.addWordsToTrie();
         }
 
         public string[] getPrediction(string word)
@@ -89,37 +118,33 @@ namespace CliApp
                     return words.ToArray();
                 }
             }
-            return [];
+            return [""];
         }
 
-        public void addWordsToTrie()
+        public string getDefinition(string word)
         {
-            if (this.words != null)
+            if (this.dic == null)
             {
-                foreach (string word in this.words)
+                throw new NullReferenceException(" Dictionary can not be null");
+            }
+            return this.dic[word];
+        }
+
+        private void addWordsToTrie()
+        {
+            if (this.dic == null)
+            {
+                throw new NullReferenceException(" Dictionary can not be null");
+            }
+            List<string> words = new List<string>(this.dic.Keys);
+            if (words.Count > 0)
+            {
+                foreach (string word in words)
                 {
                     this.trie.add(word);
                 }
             }
         }
 
-        private void getWords()
-        {
-            char[] delimiterChars = { ' ', ',', '.', ':', '\t', '[', ']', '\n', '\r', '\'', '\"', '(', ')', '{', '}' };
-            if (fileContent != null)
-            {
-                string fileContentFiltered = fileContent.Replace(@"\[\d+\]", "").ToLower();
-                this.words = fileContentFiltered.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
-            }
-        }
-
-
-        public void PrintContent()
-        {
-            if (fileContent != null)
-            {
-                Console.WriteLine(fileContent);
-            }
-        }
     }
 }
